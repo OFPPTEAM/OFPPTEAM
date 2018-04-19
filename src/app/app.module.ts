9e48fc6c9c3509b6dae79adf2a1ae772e790739f
hossine <tafaddel.hossine@gmail.com>
2018-04-19 09:24:30 +0100

    lol

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { GroupServService } from './group/group-serv.service';
+
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { GroupComponent } from './group/group.component';
@@ -159,7 +161,7 @@ const routes: Routes = [
   imports: [
     BrowserModule, RouterModule.forRoot(routes)
   ],
-  providers: [],
+  providers: [GroupServService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
